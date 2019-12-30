3885e3aad646638433bd3a4e752111572c1ac486
liyb <1055025755@qq.com>
2019-12-30 17:35:35 +0800

    doc:注释

--- app.js
+++ app.js
@@ -15,21 +15,26 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/static', express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(express.json());// express4.16.0之后版本内置了body-parser
+app.use(express.urlencoded({ extended: false }));// express4.16.0之后版本内置了body-parser
+app.use(cookieParser());// 将cookie解析为键值对
 
 // 将日志写入log目录文件中
+// 设置date为当前时区的时间
+logger.token('date', function() {
+  var p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
+  return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
+});
 var accessLogStream = rfs.createStream("access.log", {
   size: "10M", // 每10M大小轮换
   interval: "1d", // 每天轮换
   path: path.join(__dirname, "log")
 });
 app.use(logger("combined", { stream: accessLogStream }));
+
+app.use('/static', express.static(path.join(__dirname, 'public')));// 静态文件中间件
+app.use('/', indexRouter);// index路由
+app.use('/users', usersRouter);// users路由
 
 // 404错误
 app.use(function(req, res, next) {
