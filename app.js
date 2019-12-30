5f6903c683a25fc7fc5d8ae93389adb92c6c884a
liyb <1055025755@qq.com>
2019-12-30 14:13:39 +0800

    feat:logger add stream

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var rfs = require("rotating-file-stream");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// 配置ejs模板引擎
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,23 +18,31 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+// 将日志写入log目录文件中
+var accessLogStream = rfs.createStream("access.log", {
+  size: "10M", // 每10M大小轮换
+  interval: "1d", // 每天轮换
+  path: path.join(__dirname, "log")
+});
+app.use(logger("combined", { stream: accessLogStream }));
+
+// 404错误
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// 错误处理中间件
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // 仅在开发中提示错误
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // 错误页面
   res.status(err.status || 500);
   res.render('error');
 });
